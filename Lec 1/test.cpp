81deeac78fdb44d9c46aeeb79e24f946d9891b06
kalmatheroshan <kalmatheroshan@gmail.com>
2025-02-25 20:16:09 +0530

    22 patterns done

--- Lec 1/test.cpp
+++ Lec 1/test.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int sqroot(int number)
+{
+    for (int i = 2; i * i <= number; i++)
+        if (i * i == number)
+            return i;
+
+    return -1;
+}
+
 int main()
 {
 
-
-    
-    
-
+    int ans = sqroot(15);
+    cout << ans << endl;
 
     return 0;
 }
