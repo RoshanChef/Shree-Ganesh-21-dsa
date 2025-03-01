df26f9dd9f7044a4ae76e4489c6333c5e661cbc0
kalmatheroshan <kalmatheroshan@gmail.com>
2025-03-02 01:28:28 +0530

    some Questions done

--- Phase 1/01 Basics/03 bitwise manipulation.cpp
+++ Phase 1/01 Basics/03 bitwise manipulation.cpp
@@ -11,6 +11,18 @@ string check(int num, int bit)
         return "not set ";
 }
 
+int xor_n(int n)
+{
+    if (n % 4 == 0)
+        return n;
+    else if (n % 4 == 1)
+        return 1;
+    else if (n % 4 == 2)
+        return n + 1;
+    else
+        return 0;
+}
+
 int main()
 {
     // ****************** AND & ******************
@@ -101,17 +113,66 @@ int main()
     // cout << "after num : " << num << endl;
 
     // ********** NOT **********
-    int num = 16;
-    num = ~num;
-    cout << "the number is " << num << endl;
+    // int num = 16;
+    // num = ~num;
+    // cout << "the number is " << num << endl;
 
     // ********** XOR **********
-    int num1 = 5, num2 = 12;
-    cout << (num1 ^ num2) << endl;
+    // int num1 = 5, num2 = 12;
+    // cout << (num1 ^ num2) << endl;
 
     // (1) toggling/filiping the last bit & returns whole num
-    int val = 0b001001;
-    cout << (1 ^ val) << endl;
+    // int val = 0b10011;
+    // cout << (1 ^ val) << endl;
+
+    // (2). unique from pair of numbers
+    // int ar[] = {2, 1, 2, 5, 7, 5, 6, 7, 6, 1, 101};
+    // int len = sizeof(ar) / sizeof(int);
+
+    // int ans = 0;
+    // for (int i = 0; i < len; i++)
+    //     ans ^= ar[i];
+
+    // cout << "anser is : " << ans << endl;
+
+    // (3) swapping
+    // int a = 5, b = 7;
+    // cout << "before : a -> " << a << "            b -> " << b << endl;
+    // a = a ^ b;
+    // b = a ^ b;
+    // a = a ^ b;
+    // cout << "after : a -> " << a << "            b -> " << b << endl;
+
+    // // swapping
+    // int a = 5, b = 7;
+    // cout << "before : a -> " << a << "            b -> " << b << endl;
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+    // cout << "after : a -> " << a << "            b -> " << b << endl;
+
+    // (4) xor from (1,n)
+    // int n, ans = 0;
+    // cout << "enter the num ";
+    // cin >> n;
+
+    // // theta(n)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     ans ^= i;
+    // }
+    // cout << ans<<endl;
+
+    // // theta(1)
+    // cout << xor_n(n);
+
+    //  (5) xor bw (L , R)
+    // int l, r;
+    // int ans = 0;
+    // cout << "enter the range start  , end : " << endl;
+    // cin >> l >> r;
+    // ans = xor_n(l - 1) ^ xor_n(r);
+    // cout << "your answer is : " << ans;
 
     return 0;
 }
---
+++ Phase 1/03 Array/05 Reverse Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 5, b = 8;
+    cout << "before : \na => " << a << " b => " << b << endl;
+
+
+    cout << "after : \na => " << a << " b => " << b << endl;
+
+    return 0;
+}
