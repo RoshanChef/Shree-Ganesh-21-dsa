a62598b1a82abd7125cd93a787e57937035bcb42
kalmatheroshan <kalmatheroshan@gmail.com>
2025-03-11 16:21:55 +0530

    string learnt

---
+++ Phase 1/07 char & string/01 intro char.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+
+    // char ch[100];
+    // cin >> ch;
+
+    // cout << "val " << ch << endl;
+    // if (ch[5] == '\0')
+    //     cout << "yes it is " << endl;
+    // else
+    //     cout << "everything is possible " << endl;
+
+    // char ar[100];
+    // cin.getline(ar, 100);
+    // cout << "value : " << ar << endl;
+
+    // delimiter set
+    // char arr[100];
+
+    // cout << "Enter a sentence (input stops at '.'): ";
+    // cin.getline(arr, 100, '.');
+
+    // cout << "Value: " << arr << endl;
+
+    // // strcpy() — Copy one char array to another:
+    // char src[] = "World!";
+    // char dest[20];
+    // strcpy(dest, src);
+    // cout << "src : " << src << endl;
+    // cout << "dest : " << dest << endl;
+
+    // // strcat() — Concatenate (append) strings:
+    // char str1[] = "Roshan";
+    // char str2[] = "Kalmathe";
+
+    // strcat(str1, str2);
+    // cout << "str1 " << str1 << endl;
+    // cout << "str2 " << str2 << endl;
+
+    // // strcmp() — Compare two char arrays
+    // char str3[] = "Roshan";
+    // char str4[] = "Roshan";
+    // if (strcmp(str3, str4) == 0)
+    // {
+    //     cout << "Strings are equal" << endl;
+    // }
+
+    // // strstr returns substr pointer
+    // char str5[] = "Hello World";
+    // char *pos = strstr(str5, "World");
+    // cout << "Substring found: " << pos << endl; // Output: World
+
+
+    return 0;
+}
---
+++ Phase 1/07 char & string/03 string.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // strings are mutable in cpp 
+
+
+    // delimiter in string
+    // string str;
+    // cout << "Enter the value " << endl;
+    // getline(cin, str, '.');
+
+    // cout << "The value : " << str << endl;
+
+    string s;
+    // cin>> s ;
+    getline(cin, s);
+    cout << "val " << s << endl;
+
+    s = "Roshan";
+
+    if (s[6] == 0)
+    {
+        cout << "yes it is" << endl;
+    }
+
+    char ch[6] = {'b', 'a', '\0', 'b', 'a', 'r'};
+    cout << ch << endl;
+    string sc = "babbar";
+    sc[2] = 0;
+    cout << "val " << sc << endl;
+
+    sc = "roshan";
+    cout << "length : " << sc.length() << endl;
+    cout << "size : " << sc.size() << endl;
+    cout << "is empty : " << sc.empty() << endl;
+    sc.clear();
+
+    cout << "is empty : " << sc.empty() << endl;
+
+    string str = "aoshan";
+    str.push_back('N');
+    cout << str << endl;
+    str.pop_back();
+    str.pop_back();
+    cout << str << endl;
+
+    string res("ellion");
+
+    cout << res << endl;
+    cout << res.front() << endl;
+    cout << res.back() << endl;
+    cout << res.at(3) << endl;
+    cout << res << endl;
+    cout << res.substr(0, 4) << endl; // remember
+
+    // comparison
+    string str1 = "green";
+    string str2 = "green";
+    bool equal = str1 == str2;
+
+    if (equal)
+    {
+        cout << "Yes both are equal " << endl;
+    }
+
+    // another way
+    int check = str1.compare(str2); // check using minus the ascii value
+    cout << check << endl;
+
+    // find
+    string sentance = "Hello everyoneer how are you today !!";
+    string target = "everyone";
+    int index = sentance.find(target);
+    cout << "index at " << index << endl;
+
+    // replace
+    str = "This is my first Message";
+    string word = "Roshan";
+    str.replace(0, 4, word);
+    cout << str << endl;
+
+    // erase    
+    str = "Roshan is there for some reason I have";
+    cout << str.erase(1, 5) << endl;
+
+    
+    return 0;
+}
