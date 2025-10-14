a3bf6d5ae8173894e1d2107b59ec3d0bc361e2ba
kalmatheroshan <kalmatheroshan@gmail.com>
2025-10-15 00:50:40 +0530

    some topics learnt

--- Phase 2/2 OOPS/2 Basic/Types/3_multiple.cpp
+++ Phase 2/2 OOPS/2 Basic/Types/3_multiple.cpp
@@ -23,43 +23,11 @@ class smartPhone : public mobile, public laptop
 {
 };
 
-// diamond problem
-class line_a
-{
-public:
-    int physics;
-    int chemistry;
-    void print()
-    {
-        cout << "line a" << endl;
-    }
-};
-
-class line_b
-{
-public:
-    int chemistry;
-    void print()
-    {
-        cout << "line b" << endl;
-    }
-};
-
-class line_c : public line_a, public line_b
-{
-public:
-};
-
 int main()
 {
     // smartPhone vivo;
     // vivo.internet();
     // vivo.keypad();
-
-    line_c obj;
-    cout << obj.line_a::chemistry << endl;
-    obj.line_a::print();
-    obj.line_b::print();
 
     return 0;
 }
